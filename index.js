f835b9acaef828e4e40ce184a2bf1b30982a1890
abdunnoorfaheem <abdunnoor3030@gmail.com>
2024-10-01 19:55:54 +0600

    Notification Generator problem solve

--- index.js
+++ index.js
@@ -18,4 +18,22 @@ function calculateTax(income, expenses) {
 }
 
 let result = calculateTax(6000, -1500);
-console.log(result);
+// console.log(result);
+
+
+//problem--2
+
+
+
+function sendNotification(email) {
+    if (email.includes("@") === false) {
+        return "Invalid Email";
+    } else {
+        let find = email.split("@");
+        return find.join(" sent you an email from ");
+    }
+}
+
+
+let result1 = sendNotification("nadim.naem5@outlook.com");
+// console.log(result1);
